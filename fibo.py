b5965ece42cb9044b0539307a32929fdb9298a71
SimonKrutek <krutek.simon@gmail.com>
2025-04-28 09:31:26 +0200

    fibonaciho postupnost pridanie kodu

--- fibo.py
+++ fibo.py
@@ -1,5 +1,17 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n-1)+recursive_nth_fibo(n-2)
 
 def main():
+    n = int(input("zadaj pozadovany pocet prvkov:"))
+    seznam = []
+    for i in range(n):
+        seznam.append(recursive_nth_fibo(i))
+    print(seznam)
     pass
 
 
